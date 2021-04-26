3676ebd53f6336ce879846b6432973c7306fd290
AdityaKartikeyan <81302347+AdityaKartikeyan@users.noreply.github.com>
2021-04-26 16:14:41 +0530

    Added functionality and UI design
    Added functionality and UI design

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -7,7 +7,7 @@ class HeaderComponent extends Component {
         <header className="header">
           <nav className="navbar navbar-expand-lg navbar-light">
             <div>
-              <a href="http://localhost:3000/" className="navbar-brand">
+              <a href="http://localhost:3000" className="navbar-brand">
                 Apartment Security Management
               </a>
             </div>
